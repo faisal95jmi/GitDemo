ad2e4d8368cd65c9e7c4e0fdebab98c500b77b74
faisalkhan <mohammadfaisalk10@gmail.com>
2022-07-07 16:26:59 +0530

    this is commit from asian timezone guy

--- src/test/java/DataDriven.java
+++ src/test/java/DataDriven.java
@@ -20,6 +20,9 @@ public class DataDriven {
 	
 public static void main(String[] args) throws IOException
 {
+	
+	System.out.println("hello");
+	System.out.println("jello");
 		
 FileInputStream fis=new FileInputStream("D:\\Book1.xlsx");
 XSSFWorkbook workbook=new XSSFWorkbook(fis);
